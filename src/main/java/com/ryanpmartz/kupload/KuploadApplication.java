0bd75e561ed38b66489ac538227819c925906244
Ryan Martz <martzrp@gmail.com>
2017-05-20 14:48:12 -0400

    make main class a java file so IDE spring boot support works

---
+++ src/main/java/com/ryanpmartz/kupload/KuploadApplication.java
@@ -0,0 +1,13 @@
+package com.ryanpmartz.kupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuploadApplication.class, args);
+    }
+}
+
