0d1cda0d50ffc7cde900321704edd30ed6fc2987
Ryan Martz <martzrp@gmail.com>
2017-05-20 15:27:03 -0400

    add document entity

---
+++ src/main/java/com/ryanpmartz/kupload/domain/Document.java
@@ -0,0 +1,43 @@
+package com.ryanpmartz.kupload.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "documents")
+public class Document extends BaseEntity {
+
+    private User user;
+    private String url;
+    private String originalFilename;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Column(name = "original_filename")
+    public String getOriginalFilename() {
+        return originalFilename;
+    }
+
+    public void setOriginalFilename(String originalFilename) {
+        this.originalFilename = originalFilename;
+    }
+}
