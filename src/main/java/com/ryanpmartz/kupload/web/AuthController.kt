14b4278f5d693b97d89515a5a4ae64b4373d0f73
Ryan Martz <martzrp@gmail.com>
2017-05-21 21:20:22 -0400

    add auth controller

--- src/main/java/com/ryanpmartz/kupload/web/AuthController.kt
+++ src/main/java/com/ryanpmartz/kupload/web/AuthController.kt
@@ -1,15 +1,28 @@
 package com.ryanpmartz.kupload.web
 
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
 
 @Controller
 class AuthController {
 
-    @RequestMapping("/")
-    fun home(): String {
-        return "home"
+    @GetMapping("/login")
+    fun loginForm(): String {
+        return "login"
+    }
+
+    @GetMapping("/login-failed")
+    fun loginFailed(model: Model): String {
+
+        model.addAttribute("loginFailed", true)
+
+        return "login"
+    }
+
+    @GetMapping("/account/locked")
+    fun accountLocked(): String {
+        return "account-locked"
     }
 }
-
-
